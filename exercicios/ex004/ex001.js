c76c76b9daa43ea9abdc3dabdc3b01b5704a9059
Lucas Torres <repositoriolucas@gmail.com>
2022-11-20 14:14:09 -0300

    Desafio de verificação de nacionalidade

---
+++ exercicios/ex004/ex001.js
@@ -0,0 +1,10 @@
+var vel = 80.2
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+
+if (vel > 60) {  //Condição simples
+    console.log("A sua velocidade ultrapassou do limite permitido. MULTADO!")
+} else {
+    console.log("Viaje com cuidado sempre usando cinto de segurança. Boa viagem!")
+}
+
+
---
+++ exercicios/ex004/ex002.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
