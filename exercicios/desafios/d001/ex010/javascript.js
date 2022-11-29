3c2729b0293b186f1c8b300e66b3216a29e6c231
Lucas Torres <repositoriolucas@gmail.com>
2022-11-29 15:10:55 -0300

    Exercício final de vetor.

--- exercicios/desafios/d001/ex010/javascript.js
+++ exercicios/desafios/d001/ex010/javascript.js
@@ -1,5 +1,5 @@
 let num = document.getElementById('fnum')
-let lista = document.querySelector('select#flista')
+let lista = document.querySelector('select#fnum')
 let res = document.querySelector('div#res')
 let valores = []
 
@@ -21,28 +21,36 @@ function inLista(n, l) {
 
 function add() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        alert("Tudo ok!")
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert("Valor inválido ou já está na lista!")
     }
+    num.value = ''
+    num.focus()
 }
 
-function fin() {
-
-}
-
-function media() {
-
-}
-
-function maior() {
-    
-}
-
-function menor() {
-
-}
-
-function soma() {
-
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = Math.max.apply(0, valores)
+        let menor = Math.min.apply(0, valores)
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+        }
+        media += soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos <strong>${tot}</strong> número(s) cadastrado(s).</p>`
+        res.innerHTML += `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
+        res.innerHTML += `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`
+        res.innerHTML += `<p>A soma entre os valores é <strong>${soma}</strong>.</p>`
+        res.innerHTML += `<p>A média entre eles é <strong>${media}</strong>.</p>`
+    }
 }
