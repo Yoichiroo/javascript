89971364912435d4c48baed0655d6063d59e181f
Lucas Torres <repositoriolucas@gmail.com>
2022-11-29 15:43:11 -0300

    Próximos passos.

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 4] // Vetor
+let amigo = {nome: 'José', sexo: 'M', peso: 85.4, engordar(p){}} // Objeto
+console.log(amigo)
+/*
+    Amigo é o objeto
+    nome, sexo, peso são os parâmetros deste objeto.
+    Podemos adicionar funções dentro do objeto, para aprofundarmos melhor certo tipo de dado para o objeto.
+*/
