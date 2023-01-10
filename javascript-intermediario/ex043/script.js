ed5c38d9216cba08dea5fb032af30dee00ff2a6a
Lucas Torres <repositoriolucas@gmail.com>
2023-01-10 15:49:54 -0300

    Operações ternárias

---
+++ javascript-intermediario/ex043/script.js
@@ -0,0 +1,23 @@
+/*
+As operações são utilizadas pela facilidade de escrita, pela forma reduzida, assim dizendo. Podemos atribuir essa operação à uma variável.
+Para entedermos seu funcionamento, temos um exemplo.
+
+    teste lógico ? Se verdadeiro : Se falso
+    //Temos primeiro o teste lógico e as duas condicionais, caso verdadeiro ou falso.
+    
+    Exemplo prático;
+
+    let n1 = 10
+    let n2 = 5 
+
+    n1 == n2? console.log("Igual") : console.log("Desigual")
+
+*/
+
+let n1 = 10
+let n2 = 5 * 2
+let operacao = true
+
+const r = operacao ? "Verdadeiro" : "Falso"
+
+console.log(operacao && "Verdadeiro")
