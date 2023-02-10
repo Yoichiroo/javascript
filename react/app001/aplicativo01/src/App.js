4641a7a828e36d20672360d953f2c021465f421a
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 14:13:07 -0300

    Passagem de dados em React

--- react/app001/aplicativo01/src/App.js
+++ react/app001/aplicativo01/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Topo from "./componentes/header"
 import Corpo from "./componentes/corpo"
+import Dados from './componentes/dados'
 // const curso = "React.js"
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <>
     <Topo></Topo>
     <Corpo></Corpo>
+    <Dados></Dados>
     </>
   )
 }
---
+++ react/app001/aplicativo01/src/componentes/dados.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function dados() {
+    return(
+        <section>
+            <p>GitHub:</p>
+            <p>Twitter:</p>
+            <p>Curso:</p>
+        </section>
+    )
+}
