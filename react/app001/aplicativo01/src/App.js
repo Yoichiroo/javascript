2a883b2a75a58fca9867c0a8d686ef1327cb940e
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 15:23:42 -0300

    State em React - P2

--- react/app001/aplicativo01/src/App.js
+++ react/app001/aplicativo01/src/App.js
@@ -8,18 +8,12 @@ import './App.css'
 export default function App() {
   
   const [num, setNum] = useState(10)
-  let n1 = 50
-
-  const n100 = () => {
-    n1 = 100
-    console.log(n1)
-  }
 
   return (
     <>
     <Corpo></Corpo>
-    <p>Valor do SateNum: {n1}</p>
-    <button onClick={() => n100()}>100</button>
+    <p>Valor do StateNum: {num}</p>
+    <button onClick={() => setNum(num + 10)}>Soma 10</button>
     </>
   )
 }
