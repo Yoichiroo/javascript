221d3072aadae22c23ec29a17908def8d3d34e61
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 14:30:18 -0300

    Passagem de dados usando funções em React

--- react/app001/aplicativo01/src/componentes/corpo.js
+++ react/app001/aplicativo01/src/componentes/corpo.js
@@ -3,9 +3,18 @@ import Dados from './dados'
 import Yone from "./imgs/yone.jpg"
 
 export default function corpo() {
-    const gh = 'Yoichiroo'
-    const tt = '@yoichiroo1'
-    const crs = "Informática p/ Internet"
+    // const gh = 'github.com/Yoichiroo'
+    // const tt = '@yoichiroo1'
+    // const crs = "Informática p/ Internet"
+    const gh = () => {
+        return 'github.com/Yoichiroo'
+    }
+    const tt = () => {
+        return '@yoichiroo1'
+    }
+    const crs = () => {
+        return "Informática p/Internet"
+    }
     return(
         <main>
             <h2>Curso de React</h2>
@@ -15,6 +24,7 @@ export default function corpo() {
             <h1>Yone Academia de Batalha</h1>
             <h2>React é mt bom vsf</h2>
             <Dados 
+            //O atributo está carregando uma função.
                 github = {gh}  
                 twitter = {tt} 
                 curso = {crs}
--- react/app001/aplicativo01/src/componentes/dados.js
+++ react/app001/aplicativo01/src/componentes/dados.js
@@ -5,9 +5,10 @@ export default function dados(props) {
     //Este componente está preparado para utilizar atributos que serão passados para o mesmo.
     return(
         <section>
-            <p><strong>GitHub:</strong> {props.github}</p>
-            <p><strong>Twitter:</strong> {props.twitter}</p>
-            <p><strong>Curso:</strong> {props.curso}</p>
+             {/* Aqui o atributo está carregando o retorno da função */}
+            <p><strong>GitHub:</strong> {props.github()}</p>
+            <p><strong>Twitter:</strong> {props.twitter()}</p>
+            <p><strong>Curso:</strong> {props.curso()}</p>
         </section>
     )
 }
