c69cfd4b1f1c4a16dc12b2cd8abf56d50b20591a
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 14:02:55 -0300

    Criado o topo do aplicativo

--- react/app001/aplicativo01/src/App.js
+++ react/app001/aplicativo01/src/App.js
@@ -1,26 +1,11 @@
 import React from "react"
-import Irelia from './componentes/imgs/irelia.jpg'
+import Topo from "./componentes/header"
 // const curso = "React.js"
 
 export default function App() {
-  const gitHub = () => {
-    return("Yoichiroo")
-  }
-
-  function curso() {
-    return 'React.js'
-  }
-
   return (
     <>
-    <header>
-      <h1> {"GitHub - " + gitHub()}</h1>
-      <p>{"Curso - " + curso()}</p>
-    </header>
-    <section>
-      <img src={Irelia} alt = "Irelia" width = "1000"></img>
-      <img src = 'img/yone.jpg' alt='"Yone' width = "1000"></img>
-    </section>
+    <Topo></Topo>
     </>
   )
 }
---
+++ react/app001/aplicativo01/src/componentes/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Irelia from "./imgs/irelia.jpg"
+
+export default function header(){
+    return(
+        <header>
+            <img src={Irelia} alt = "Irelia Sentinela" width="1000"></img>
+            <h1>Irelia Sentinela</h1>
+        </header>
+    )
+}
