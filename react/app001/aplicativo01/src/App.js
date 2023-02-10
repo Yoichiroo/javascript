7d966844e8b8c6796e79f4b85a0441420e2091e1
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 15:20:57 -0300

    State em React

--- react/app001/aplicativo01/src/App.js
+++ react/app001/aplicativo01/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import Corpo from "./componentes/corpo"
 import './App.css'
 
@@ -7,9 +7,19 @@ import './App.css'
 
 export default function App() {
   
+  const [num, setNum] = useState(10)
+  let n1 = 50
+
+  const n100 = () => {
+    n1 = 100
+    console.log(n1)
+  }
+
   return (
     <>
     <Corpo></Corpo>
+    <p>Valor do SateNum: {n1}</p>
+    <button onClick={() => n100()}>100</button>
     </>
   )
 }
