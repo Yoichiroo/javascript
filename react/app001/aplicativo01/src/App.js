fb8f23b8680fbd161ed80dbd0507032660c71a0c
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 14:05:16 -0300

    Criando o corpo do aplicativo

--- react/app001/aplicativo01/src/App.js
+++ react/app001/aplicativo01/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Topo from "./componentes/header"
+import Corpo from "./componentes/corpo"
 // const curso = "React.js"
 
 export default function App() {
   return (
     <>
     <Topo></Topo>
+    <Corpo></Corpo>
     </>
   )
 }
--- react/app001/aplicativo01/src/componentes/corpo.js
+++ react/app001/aplicativo01/src/componentes/corpo.js
@@ -1,1 +1,11 @@
-import 
+import React from "react"
+
+export default function corpo() {
+    return(
+        <main>
+            <h2>Curso de React</h2>
+            <p>Jogue LoL, é viciante!</p>
+            <p>Mono Yone falando aqui.</p>
+        </main>
+    )
+}
