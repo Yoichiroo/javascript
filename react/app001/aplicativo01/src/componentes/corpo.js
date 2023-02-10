97673120174044297176fda4b5d09d928d2d476b
Lucas Torres <repositoriolucas@gmail.com>
2023-02-10 14:43:28 -0300

    CSS com constantes em React

--- react/app001/aplicativo01/src/componentes/corpo.js
+++ react/app001/aplicativo01/src/componentes/corpo.js
@@ -30,7 +30,7 @@ export default function corpo() {
             <p>Jogue LoL, é viciante!</p>
             <p>Mono Yone falando aqui.</p>
             <img src={Yone} alt="Yone Academia de Batalha" width="1000"></img>
-            <h1>Yone Academia de Batalha</h1>
+            <h1 style ={textoDestaque}>Yone Academia de Batalha</h1>
             <h2>React é mt bom!</h2>
             <Dados 
             //O atributo está carregando uma função.
