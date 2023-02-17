97632820208ae14e158d1547e0f34e66594c3e65
Lucas Torres <repositoriolucas@gmail.com>
2023-02-17 19:09:07 -0300

    Estrutura básica

--- react/app004/aplicativo04/src/App.js
+++ react/app004/aplicativo04/src/App.js
@@ -9,10 +9,24 @@ export default function App() {
   const verde = {color: "green"}
   const azul = {color: "blue"}
   
+  const retornarCor = (c) => {
+    if (c == 1) {
+      return vermelho
+    } else if (c == 2) {
+      return verde
+    } else {
+      return azul
+    }
+  }
+
+  const mudarCor = () => {
+
+  }
+
   return (
     <>
-    <h1 style={vermelho}>Yoichiroo</h1>
-    <button onClick=''>Mudar Cor</button>
+    <h1 style={retornarCor()}>Yoichiroo</h1>
+    <button onClick={() => mudarCor()}>Mudar Cor</button>
     </>
   )
 }
