ad7ae394b2ac57d7132e8af7a7d17a016d7413fa
Lucas Torres <repositoriolucas@gmail.com>
2023-02-17 18:41:05 -0300

    Renderizações condicionais em React

--- react/app003/aplicativo03/src/App.js
+++ react/app003/aplicativo03/src/App.js
@@ -1,8 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
 export default function App() {
+
+  const [log, setLog] = useState(false)
+
+  const mensagemLogIn = () => {
+    return 'Usuário logado'
+  }
+
+  const mensagemLogOff = () => {
+    return 'Por favor, logar no sistema.'
+  }
+
+  const comprimento = () => {
+    const hora = new Date().getHours()
+    if (hora >= 0 && hora < 12) {
+      return <p>Bom dia!</p>
+    } else if (hora >= 13 && hora < 18) {
+      return <p>Boa tarde!</p>
+    } else {
+      return <p>Boa noite!</p>
+    }
+  }
+
   return (
     <>
-    <h1>Renderização Condicional</h1> 
+    <h1>Renderização Condicional</h1>
+    {comprimento()}
+    <p>{log ? mensagemLogIn() : mensagemLogOff() }</p>
+    <button onClick={() => setLog(!log)}>{log ? "LogOff" : "LogIn"}</button>
     </>
   )
 }
