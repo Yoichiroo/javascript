4027011860895d3af3c49c770a034cf3786e9f0d
Lucas Torres <repositoriolucas@gmail.com>
2023-02-17 17:38:33 -0300

    Eventos em React

--- react/app002/aplicativo02/src/App.js
+++ react/app002/aplicativo02/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Yone from './componentes/img/yone.jpg'
+import Sylas from './componentes/img/sylas.jpg'
+import { useState } from 'react'
 
 export default function App() {
   return (
     <>
     <h1>Segundo app - Eventos</h1>
+    <p>Meus mains: </p>
+    <img src = {Yone} alt="Yone Emissário da Luz" style={{width:'1000px', display: "block"}}></img>
+    <button>Mudar</button>
     </>
   )
 }
