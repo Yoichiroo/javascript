9b1f9b40fbb835534a882a7565f89f369da36cdf
Lucas Torres <repositoriolucas@gmail.com>
2023-02-17 18:07:59 -0300

    Cancelamento de função em React

--- react/app002/aplicativo02/src/App.js
+++ react/app002/aplicativo02/src/App.js
@@ -5,9 +5,14 @@ export default function App() {
 
   const [mudar, setMudar] = useState(false)
 
+  const cancelar = (obj) => {
+    return obj.preventDefault()
+  }
+
   return (
     <>
     <Main mudar = {mudar} setMudar={setMudar}></Main>
+    <a href="http://github.com/Yoichiroo" target={"_blank"}  rel="noreferrer" onClick= {(elemento) => cancelar(elemento)} style={{display:"block", color: "black", textDecoration: "none", fontSize: "1.2em", fontWeight:"bold"}}>GitHub</a>
     </>
   )
 }
