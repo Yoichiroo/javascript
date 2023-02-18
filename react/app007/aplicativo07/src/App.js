4679e4c75d0bf8a5b23daf7501685fb5f9d0ede4
Lucas Torres <repositoriolucas@gmail.com>
2023-02-18 16:08:48 -0300

    Utilizando um state funcionado para todos os elementos.

--- react/app007/aplicativo07/src/App.js
+++ react/app007/aplicativo07/src/App.js
@@ -2,17 +2,31 @@ import React, {useState} from "react"
 
 export default function App() {
 
-  const [form, setForm] = useState('')
+  const [form, setForm] = useState({"nome": "", "curso": "", "ano": ""})
+
+  const manipularFormChange = (elemento) => {
+    if(elemento.target.getAttribute('name') == "uNome") {
+      setForm({"nome": elemento.target.value, "curso": form.curso, "ano": form.ano})
+    } else if (elemento.target.getAttribute('name') == "uCurso") {
+      setForm({"nome": form.nome, "curso": elemento.target.value, "ano": form.ano})
+    } else if (elemento.target.getAttribute('name') == "uAno"){
+      setForm({"nome": form.nome, "curso": form.curso, "ano": elemento.target.value})
+    }
+  }
 
   return (
     <>
     <h1>Formulário com React</h1>
     <label for="uNome">Nome</label>
-    <input type="text" name = "uNome" value={} onChange={}></input>
+    <input type="text" name = "uNome" value = {form.nome} onChange={(elemento) => manipularFormChange(elemento)}></input>
     <label for="uCurso">Curso</label>
-    <input type="text" name = "uCurso" value={} onChange={}></input>
+    <input type="text" name = "uCurso" value = {form.curso} onChange={(elemento) => manipularFormChange(elemento)}></input>
     <label for="uAno">Ano</label>
-    <input type="text" name = "uAno" value={} onChange={}></input>
+    <input type="text" name = "uAno" value = {form.ano} onChange={(elemento) => manipularFormChange(elemento)}></input>
+    
+    <p>Nome digitado - <strong>{form.nome}</strong></p>
+    <p>Curso digitado - <strong>{form.curso}</strong></p>
+    <p>Ano digitado - <strong>{form.ano}</strong></p>
     </>
   )
 }
