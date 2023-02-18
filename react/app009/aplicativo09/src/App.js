c5c94a7f0f7d3c8ebf1299ca15a65f79d6f59939
Lucas Torres <repositoriolucas@gmail.com>
2023-02-18 17:19:48 -0300

    Contenção em React

---
+++ react/app009/aplicativo09/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function App() {
+  
+
+
+  return(
+    <>
+    <div>
+      <h1>Contenção em React</h1>
+      <p>Curso de React</p>
+    </div>
+    </>
+  )
+}
