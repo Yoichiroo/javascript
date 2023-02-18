0c6b42a664c27bd902a86d2b72f9a7d1fb0c0e54
Lucas Torres <repositoriolucas@gmail.com>
2023-02-18 14:59:40 -0300

    Criando listas utilizando a função map.

--- react/app005/aplicativo05/src/App.js
+++ react/app005/aplicativo05/src/App.js
@@ -1,7 +1,18 @@
 import React from "react"
 
 export default function App() {
+
+  const carros = ["HRV", "Golf", "Focus", "Cruze", "Argo", "Camaro", "Rover"]
+
+  const listaCarros = carros.map(
+    (carro)=> 
+        <li>{carro}</li> 
+  )
+
   return (
-    
+    <>
+    <h1>Listas usando a função Map</h1>
+    <ul>{listaCarros}</ul>
+    </>
   )
 }
