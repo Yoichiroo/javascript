ae123e03487152ccbdbe2aadea72a0312c6c58c0
Lucas Torres <repositoriolucas@gmail.com>
2023-02-18 15:07:20 -0300

    Função map sendo utilizada com JSON

--- react/app005/aplicativo05/src/App.js
+++ react/app005/aplicativo05/src/App.js
@@ -2,11 +2,18 @@ import React from "react"
 
 export default function App() {
 
-  const carros = ["HRV", "Golf", "Focus", "Cruze", "Argo", "Camaro", "Rover"]
+  const carros = [
+    {categoria: "Esporte", preco: "110.000,00", modelo: "Golf GTI"},
+    {categoria: "Esporte", preco: "120.000,00", modelo: "Camaro"},
+    {categoria: "SUV", preco: "85.000,00", modelo: "HRV"},
+    {categoria: "SUV", preco: "83,000.00", modelo: "T-Cross"},
+    {categoria: "Utilitário", preco: "120.000,00", modelo: "Hillux"},
+    {categoria: "Utilitário", preco: "90.000,00", modelo: "Ranger"},
+  ]
 
   const listaCarros = carros.map(
-    (carro)=> 
-        <li>{carro}</li> 
+    (carro, indice) => 
+        <li key={indice}> {carro.modelo} - <strong>{carro.categoria}</strong> - R$ {carro.preco}</li> 
   )
 
   return (
