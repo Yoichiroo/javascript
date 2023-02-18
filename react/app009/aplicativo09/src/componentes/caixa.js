7eca001d60fe403da8c591ebe13fe65fc0f9d7f0
Lucas Torres <repositoriolucas@gmail.com>
2023-02-18 17:29:08 -0300

    Entendendo melhor o funcionamento da contenção com componentes externos

--- react/app009/aplicativo09/src/componentes/caixa.js
+++ react/app009/aplicativo09/src/componentes/caixa.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Github from "./github"
 
 export default function caixa(props) {
     return(
         <>
-        <h1>Yoichiroo</h1>
+        <Github></Github>
         <p>{props.site}</p>
         {/* O props.children irá retornar e colocar em posição todos os elementos filho do componente caixa, podemos especificar em formato de Array qual elemento queremos retornar.*/}
         {props.children}
