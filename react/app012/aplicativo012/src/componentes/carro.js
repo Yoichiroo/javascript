4da7b2558aa518849e86971cdf215f4ad2b0348b
Lucas Torres <repositoriolucas@gmail.com>
2023-02-19 16:24:19 -0300

    Usando STATE em componentes de classe

--- react/app012/aplicativo012/src/componentes/carro.js
+++ react/app012/aplicativo012/src/componentes/carro.js
@@ -5,6 +5,10 @@ export default class Carro extends React.Component {
     constructor(props) {
         super(props)
         this.modelo = "Golf"
+        this.state = {
+            ligado: false, 
+            velocidadeAtual: 0
+        }
     }
     
     render() {
@@ -12,6 +16,9 @@ export default class Carro extends React.Component {
             <>
             <div>
                 <h2>Meu Carro</h2>
+                <p>Modelo: <strong>{this.modelo}</strong></p>
+                <p>Ligado: <strong>{this.state.ligado ? "Sim" : "Não"}</strong></p>
+                <p>Vel. Atual: <strong>{this.state.velocidadeAtual}</strong></p>
             </div>
             </>
         )
