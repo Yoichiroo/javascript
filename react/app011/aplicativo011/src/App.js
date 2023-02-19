55da38939a014436d8698a315a3b9d31bb2fd3b2
Lucas Torres <repositoriolucas@gmail.com>
2023-02-19 15:44:11 -0300

    Update App.js

--- react/app011/aplicativo011/src/App.js
+++ react/app011/aplicativo011/src/App.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 export default function App() {
 
-  localStorage.setItem("nome", "Lucas") // DEFINIÇÃO
-  localStorage.getItem("nome") // OBTENÇÃO
-  localStorage.removeItem("nome") // REMOÇÃO
+  
 
   return(
     <>
