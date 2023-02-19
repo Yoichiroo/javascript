0ac73acda92d311163ba7025e9b7cc688965e7f0
Lucas Torres <repositoriolucas@gmail.com>
2023-02-19 15:47:36 -0300

    Estrutura básica para manipulação de componentes de formulário com LocalStorage

--- react/app011/aplicativo011/src/App.js
+++ react/app011/aplicativo011/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function App() {
 
-  
+  const [nome, setNome] = useState()
+
+  const armazenar = (chave, valor) => {
+    localStorage.setItem(chave, valor)
+  }
+
+  const consultar = (chave) => {
+    alert(localStorage.getItem(chave))
+  }
+
+  const apagar = (chave) => {
+    localStorage.removeItem(chave)
+  }
 
   return(
     <>
+      <label>Digite um Nome: </label>
+      <input type="text" value={nome} onChange={(elemento) => setNome(elemento.target.value)}></input>
     </>
   )
 }
