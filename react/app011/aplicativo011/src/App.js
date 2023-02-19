9066a6866d6a9889057bc574b52b01ed202af2a1
Lucas Torres <repositoriolucas@gmail.com>
2023-02-19 15:43:20 -0300

    Métodos básicos do LocalStorage

--- react/app011/aplicativo011/src/App.js
+++ react/app011/aplicativo011/src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function App() {
 
-  
+  localStorage.setItem("nome", "Lucas")
+  localStorage.getItem("nome")
+  localStorage.removeItem("nome")
 
   return(
     <>
-    
     </>
   )
 }
