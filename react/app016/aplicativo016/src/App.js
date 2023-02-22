dc460177272b11c816a336d10b7e7987a483f114
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 16:08:08 -0300

    Função que retornará as páginas

--- react/app016/aplicativo016/src/App.js
+++ react/app016/aplicativo016/src/App.js
@@ -4,10 +4,24 @@ import Pag2 from "./componentes/pag002"
 
 export default function App() {
   
+  const [pagina, setPagina] = useState(0)
+
+  const retornarPagina = () => {
+    if (pagina == 1) {
+      return <Pag1></Pag1>
+    } else if (pagina == 2) {
+      return <Pag2></Pag2>
+    } else {
+      <div>
+      <button onClick="">Página 1</button>
+      <button onClick="">Página 2</button>
+      </div>
+    }
+  }
+
   return(
     <>
-      <button onClick={""}>Página 1</button>
-      <button onClick={""}>Página 2</button>
+      {retornarPagina()}
     </>
   )
 }
