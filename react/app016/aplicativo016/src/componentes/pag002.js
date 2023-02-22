89ec6dbd0cadee781efd4cf3316dc5d8fd1ab7ef
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 16:01:00 -0300

    Criando as páginas para a navegação

--- react/app016/aplicativo016/src/componentes/pag002.js
+++ react/app016/aplicativo016/src/componentes/pag002.js
@@ -4,8 +4,8 @@ export default function App() {
     
     return(
         <div>
-            <h1>Página 1</h1>
-            <h2>Curso de React</h2>
+            <h1>Página 2</h1>
+            <h2>Yoichiroo</h2>
         </div>
     )
 }
