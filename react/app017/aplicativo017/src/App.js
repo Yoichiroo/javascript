ef32630b3d7ad3d53cc9d7e8880cc7bae381dfa5
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 16:42:56 -0300

    Estrutura básica para a lista de carros

--- react/app017/aplicativo017/src/App.js
+++ react/app017/aplicativo017/src/App.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, {useState} from "react"
+
+const carros = [
+  {categoria:"Esportivo", preco: "110.000,00", modelo: "Golf GTI"},
+  {categoria:"Esportivo", preco: "120.500,00", modelo: "Camaro"},
+  {categoria:"SUV", preco: "85.000,00", modelo: "HRV"},
+  {categoria:"SUV", preco: "90.000,00", modelo: "T-Cross"},
+  {categoria:"Utilitário", preco: "125.000,00", modelo: "Hillux"},
+  {categoria:"Utilitário", preco: "60.000,00", modelo: "Ranger"}
+]
+
+const tabelaCarros = (cat) => {
+
+}
 
 export default function App() {
+
+  const [categoria, setCategoria] = useState("")
+
   return(
     <>
-      
+    {tabelaCarros(categoria)}
     </>
   )
 }
