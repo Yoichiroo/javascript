61419aab59822aefcec9980763fadde24dda04a8
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 15:45:49 -0300

    Manipulando variáveis globais

--- react/app015/aplicativo015/src/App.js
+++ react/app015/aplicativo015/src/App.js
@@ -4,13 +4,24 @@ import Info from "./componentes/info"
 
 export default function App() {
   
+  const [resumo, setResumo] = useState(Globais.resumo)
+
+  const gravarResumo = () => {
+    Globais.resumo = resumo
+  }
+
+  const verResumo = () => {
+    alert(Globais.resumo)
+  }
+
   return (
     <>
+    <Info></Info>
     <p>{'Github: ' + Globais.github}</p>
     <p>{'Curso: ' + Globais.curso}</p>
     <p>{'Ano: ' + Globais.ano}</p>
     <hr></hr>
-    <Info></Info>
+    <input type="text" value = {""} onChange = {""}></input>
     </> 
   )
 }
--- react/app015/aplicativo015/src/componentes/globais.js
+++ react/app015/aplicativo015/src/componentes/globais.js
@@ -4,4 +4,5 @@ export default class Globais{
     static github = "Yoichiroo"
     static curso = "Curso de React"
     static ano = 2023
+    static resumo = "Curso de React no GitHub Yoichiroo"
 }
