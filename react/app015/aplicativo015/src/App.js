463d546d09c9f7350eb90b0b397999f9367b22ad
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 15:48:52 -0300

    Entendendo o funcionamento das variáveis globais

--- react/app015/aplicativo015/src/App.js
+++ react/app015/aplicativo015/src/App.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react'
 import Globais from "./componentes/globais"
+import Info from "./componentes/info"
 
 export default function App() {
   
   return (
     <>
-    
+    <p>{'Github: ' + Globais.github}</p>
+    <p>{'Curso: ' + Globais.curso}</p>
+    <p>{'Ano: ' + Globais.ano}</p>
+    <hr></hr>
+    <Info></Info>
     </> 
   )
 }
---
+++ react/app015/aplicativo015/src/componentes/info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Globais from "./globais"
+
+export default function App() {
+    
+    return (
+        <>
+        <p>{'Github: ' + Globais.github}</p>
+        <p>{'Curso: ' + Globais.curso}</p>
+        <p>{'Ano: ' + Globais.ano}</p>
+        <hr></hr>
+        </> 
+    )
+}
