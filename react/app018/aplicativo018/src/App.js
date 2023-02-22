030735ae3bbaccb9d6cfa4ccfc38bcf6854b4ffc
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 17:54:10 -0300

    Calculadora pronta

--- react/app018/aplicativo018/src/App.js
+++ react/app018/aplicativo018/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import CSS from './App.css'
+import './App.css'
 
 const tabelaExibicaoIMC = () => {
   return (
@@ -13,27 +13,27 @@ const tabelaExibicaoIMC = () => {
       <tbody>
         <tr>
           <td>Abaixo do peso</td>
-          <td>Abaixo de 18,5</td>
+          <td><strong>Abaixo de 18,5</strong></td>
         </tr>
         <tr>
           <td>Peso Normal</td>
-          <td>Entre 18,5 e 24,9</td>
+          <td><strong>Entre 18,5 e 24,9</strong></td>
         </tr>
         <tr>
           <td>Sobrepeso</td>
-          <td>Entre 25 e 29,9</td>
+          <td><strong>Entre 25 e 29,9</strong></td>
         </tr>
         <tr>
           <td>Obesidade Grau I</td>
-          <td>Entre 30 e 34,9</td>
+          <td><strong>Entre 30 e 34,9</strong></td>
         </tr>
         <tr>
           <td>Obesidade Grau II</td>
-          <td>Entre 35 e 39,9</td>
+          <td><strong>Entre 35 e 39,9</strong></td>
         </tr>
         <tr>
           <td>Obesidade Grau III ou Mórbida</td>
-          <td>Maior que 40</td>
+          <td><strong>Maior que 40</strong></td>
         </tr>
       </tbody>
     </table>
@@ -43,6 +43,7 @@ const tabelaExibicaoIMC = () => {
 const userPeso = (peso, sPeso) => {
   return(
     <div>
+      <label>Peso: </label>
       <input type="text" value={peso} onChange={(elemento) => sPeso(elemento.target.value)}></input>
     </div>
   )
@@ -51,17 +52,43 @@ const userPeso = (peso, sPeso) => {
 const userAltura = (altura, sAltura) => {
   return(
     <div>
+      <label>Altura: </label>
       <input type="text" value={altura} onChange={(elemento) => sAltura(elemento.target.value)}></input>
+    </div>
+  )
+}
+
+const calculoIMC = (peso, altura, setRes) => {
+  const calculo = () => {
+    setRes(peso / (altura * altura))
+  }
+  return (
+    <div>
+      <button onClick={calculo}>Calcular</button>
+    </div>
+  )
+}
+
+const uRes = (resultado) => {
+  return (
+    <div>
+      <p>Resultado: {resultado.toFixed(2)}</p>
     </div>
   )
 }
 
 export default function App() {
   
+  const [peso, setPeso] = useState(0)
+  const [altura, setAltura] = useState(0)
+  const [res, setResultado] = useState(0)
 
-  
   return(
     <>
+    {userPeso(peso, setPeso)}
+    {userAltura(altura, setAltura)}
+    {calculoIMC(peso, altura, setResultado)}
+    {uRes(res)}
     {tabelaExibicaoIMC()}
     </>
   )
