6fa50b88116d7c0f027aa5011336f0bfbec2ad6d
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 17:24:50 -0300

    Estrutura básica da tabela com as informações do IMC

--- react/app018/aplicativo018/src/App.js
+++ react/app018/aplicativo018/src/App.js
@@ -4,7 +4,38 @@ import CSS from './App.css'
 const tabelaExibicaoIMC = () => {
   return (
     <table className="tabela">
-
+      <thead>
+        <tr>
+          <th>Classificação</th>
+          <th>IMC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Abaixo do peso</td>
+          <td>Abaixo de 18,5</td>
+        </tr>
+        <tr>
+          <td>Peso Normal</td>
+          <td>Entre 18,5 e 24,9</td>
+        </tr>
+        <tr>
+          <td>Sobrepeso</td>
+          <td>Entre 25 e 29,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau I</td>
+          <td>Entre 30 e 34,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau II</td>
+          <td>Entre 35 e 39,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau III ou Mórbida</td>
+          <td>Maior que 40</td>
+        </tr>
+      </tbody>
     </table>
   )
 }
