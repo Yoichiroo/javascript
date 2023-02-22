246e6f784e7b78ae6f66fafb48251d6bb561612e
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 15:36:54 -0300

    Manual completo de Componentes de classe com comentários

--- react/app014/aplicativo014/src/componentes/classeBase.js
+++ react/app014/aplicativo014/src/componentes/classeBase.js
@@ -1,22 +1,32 @@
 import React from "react"
 
 export default class classeBase extends React.Component {
+    //Permitindo o Uso de Props
     constructor(props) {
-        super(props)
         //Instruções básicas do construtor
+        super(props)
+
+        //State
         this.state = {
             github: "Yoichiroo",
             curso: 'React',
             ativo: true,
             nome: this.props.nomeAluno
         }
+
         this.status = this.props.status
+
+        //Bindagem
+
         let AD = this.ativarDesativar.bind(this)
     }
 
+    //Função para manipulação de State
     ativarDesativar() {
         this.setState(state => ({ativo: !state.ativo}))
     }
+
+    //Métodos de ciclo de vida dos componentes
 
     componentDidMount() {
         console.log("O componente foi criado")
@@ -34,6 +44,10 @@ export default class classeBase extends React.Component {
         return(
             <>
             <h1>Componente de Classe</h1>
+            {/*Chamada da função com bind */}
+            <button onClick={this.AD}>Ativar Desativar</button>
+            {/* Chamada da função sem bind */}
+            <button onClick={() => this.ativarDesativar}>Ativar Desativar</button>
             </>
         )
     }
