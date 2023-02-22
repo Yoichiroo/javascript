72fdb96c5f79afb77cc815a2410931674af5ae04
Lucas Torres <repositoriolucas@gmail.com>
2023-02-22 15:25:36 -0300

    States em componentes de classe

--- react/app014/aplicativo014/src/componentes/classeBase.js
+++ react/app014/aplicativo014/src/componentes/classeBase.js
@@ -3,6 +3,11 @@ import React from "react"
 export default class classeBase extends React.Component {
     constructor(props) {
         super(props)
+        //Instruções básicas do construtor
+        this.state = {
+            github: "Yoichiroo",
+            curso: 'React'
+        }
     }
 
     return ()
