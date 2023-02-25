bb6f001b7c602e76526210bf9821c6c49571cb43
Lucas Torres <repositoriolucas@gmail.com>
2023-02-25 17:03:53 -0300

    Criando as variáveis com o CSS da tela da calculadora

--- react/app019/aplicativo019/src/App.js
+++ react/app019/aplicativo019/src/App.js
@@ -1,9 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 
 export default function App() {
+  
+  const [valTela, setValTela] = useState("")
+  const [resultado, setResultado] = useState(0)
+  const [acumulador, setAcumulador] = useState(0)
+  const [operacao, setOperacao] = useState(false)
+
+  const Tela = (valor, res) => {
+    return(
+      <div>
+        <span>{valor}</span>
+        <span>{res}</span>
+      </div>
+    )
+  }
+
+  //Estilos
+  const cssTela = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    width: 260,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: "#444"
+  }
+
+  const cssTelaOperacao = {
+    fontSize: 25,
+    color: "#fff",
+    height: 20,
+  }
+
+  const cssTelaResultado = {
+    
+  }
+
   return(
     <>
-      
+
     </>
   )
 }
